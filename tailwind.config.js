c6f0f473c2face24f57f0d74ab0c21744a5bde47
Eric Mugo <62234060+ericlyon2020@users.noreply.github.com>
2025-06-17 01:19:23 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// File: tailwind.config.js
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        lavender: '#E6E6FA',
+      },
+    },
+  },
+  plugins: [],
+};
