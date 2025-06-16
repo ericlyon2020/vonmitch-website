063d655c9d68e05350ca52eac087472572bd0c43
Eric Mugo <62234060+ericlyon2020@users.noreply.github.com>
2025-06-17 01:39:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-// File: vite.config.js
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@supabase/supabase-js']
+    }
+  }
 });
